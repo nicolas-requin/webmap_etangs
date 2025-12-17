97380256ca3131831316b24565eecbea28b39780
nicolas-requin <nicolas.requin81@gmail.com>
2025-12-17 09:48:34 +0100

    Création du Git et upload fichiers

---
+++ js/map.js
@@ -0,0 +1,27 @@
+const map = new maplibregl.Map({
+  container: 'map',
+  style: {
+    version: 8,
+    sources: {
+      osm: {
+        type: 'raster',
+        tiles: [
+          'https://a.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png',
+          'https://c.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        ],
+        tileSize: 256,
+        attribution: '© OpenStreetMap contributors'
+      }
+    },
+    layers: [
+      {
+        id: 'osm',
+        type: 'raster',
+        source: 'osm'
+      }
+    ]
+  },
+  center: [1.2100, 46.7561],
+  zoom: 10
+});
